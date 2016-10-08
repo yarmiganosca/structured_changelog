e14db48aa888cf9075b7ebdfee321d2484f57d6f
Chris Hoffman <choffman@optoro.com>
2016-10-08 17:24:20 -0400

    fixed specs

--- spec/structured_changelog_spec.rb
+++ spec/structured_changelog_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe StructuredChangelog do
   subject(:changelog)  { StructuredChangelog.new(changelog_file) }
 
   describe "#version" do
-    it "is the versions number in the first RELEASE block" do
+    it "is the version number in the first RELEASE block" do
       expect(changelog.version).to eq "0.2.0"
     end
   end
 
   describe "#roadmap" do
     it 'contains all the versions in the ROADMAP blocks' do
-      expect(changelog.roadmaps.map(&:version)).to eq %w(100.0.0 100.0.1)
+      expect(changelog.roadmaps.map(&:version).sort).to eq %w(100.0.0 100.0.1)
     end
   end
 end
