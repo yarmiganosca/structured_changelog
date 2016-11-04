4b61ec84f51512416db671bf5e9f6a3936e4ac3d
Chris Hoffman <choffman@optoro.com>
2016-11-04 12:22:59 -0400

    created a tasks spec directory

--- spec/structured_changelog/tasks/validate_spec.rb
+++ spec/structured_changelog/tasks/validate_spec.rb
@@ -1,20 +1,18 @@
 require 'fantaskspec'
-require 'structured_changelog/task'
+require 'structured_changelog/tasks/validate'
 
-RSpec.describe 'changelog:validate', type: :rake do
+RSpec.describe 'rake changelog:validate', type: :rake do
   let(:task_name) { 'changelog:validate' }
 
   it 'validates a good changelog' do
-    options = {path: Pathname.new(__FILE__)/'..'/'..'/'CHANGELOG.md'}
+    options = {path: Pathname.new(__FILE__)/'..'/'..'/'..'/'CHANGELOG.md'}
 
     expect { task.execute(options) }.to output(/Valid /).to_stdout
   end
 
   it "doesn't validate a bad changelog" do
-    options = {path: Pathname.new(__FILE__)/'..'/'..'/'BADLOG.md'}
+    options = {path: Pathname.new(__FILE__)/'..'/'..'/'..'/'BADLOG.md'}
 
     expect { task.execute(options) }.to output(/No RELEASE blocks/).to_stdout
   end
 end
-
-RSpec.describe ''
