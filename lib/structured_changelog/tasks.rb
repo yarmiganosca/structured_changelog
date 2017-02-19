030f55539bba1efbf91f2d06c03ce8aa88bb32e4
Chris Hoffman <choffman@optoro.com>
2017-02-19 17:44:55 -0500

    turns out build is the task we want to enhance

--- lib/structured_changelog/tasks.rb
+++ lib/structured_changelog/tasks.rb
@@ -1,7 +1,5 @@
 Dir[File.join(File.dirname(__FILE__), 'tasks', '*.rb')].each(&method(:require))
 
-if Rake::Task.task_defined?('release:guard_clean')
-  Rake::Task['release:guard_clean'].enhance %w(changelog:validate changelog:sync changelog:commit)
-elsif Rake::Task.task_defined?('release')
-  Rake::Task['release'].enhance %w(changelog:validate changelog:sync changelog:commit)
+if Rake::Task.task_defined?('build')
+  Rake::Task['build'].enhance %w(changelog:validate changelog:sync changelog:commit)
 end
