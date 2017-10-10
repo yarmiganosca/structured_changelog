cdae4d6cbe6f13452e695ab922bb633d56d4d98d
Chris Hoffman <choffman@optoro.com>
2017-10-10 14:08:28 -0400

    typo

--- lib/structured_changelog/tasks/sync.rb
+++ lib/structured_changelog/tasks/sync.rb
@@ -4,7 +4,7 @@ require 'structured_changelog/version_pattern'
 desc "Set the gem's VERSION constant to be the changelog's version"
 task 'changelog:sync', [:changelog_path, :version_path] do |_task, arguments|
   changelog_path = arguments.to_h.fetch(:changelog_path) { "CHANGELOG.md" }
-  version_path   = arguments.to_h.fetch(:version_path)   { "lib/#{Pathname.pwd.basename}/version.rb"}
+  version_path   = arguments.to_h.fetch(:version_path)   { "lib/#{Pathname.pwd.basename}/version.rb" }
 
   changelog    = StructuredChangelog.new(changelog_path)
   version_file = Pathname.new(version_path)
